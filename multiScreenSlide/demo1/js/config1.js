1582dc2f40e83a27c5a5c465fa70018ad52c3999
aimei <657074944@qq.com>
2017-01-12 11:39:25 +0800

    first commit

---
+++ multiScreenSlide/demo1/js/config1.js
@@ -0,0 +1,5 @@
+;(function(){var params= {"title":"QQ空间5.0——多彩年轻、乐享生活","summary":"多彩年轻、乐享生活，我是第#XXX#位预约QQ空间手机版5.0的用户。","summary_no":"QQ空间手机版5.0发布啦，抢先体验吧！","pic":"http://qzonestyle.gtimg.cn/aoi/sola/20141204111648_xOmCLs9Dl7.jpg?r=124564567498","url":"http://m.qzone.com/l?g=1335"};
+typeof callback_31_config1 === "function" && callback_31_config1(params)
+window.define && define(function (require, exports, module) {
+return params});
+})()
